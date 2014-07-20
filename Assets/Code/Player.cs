fab184e27a2e2d54ec44382827377de3e93d12a3
dpoljak <poljak5@gmail.com>
2014-07-20 13:03:28 +0200

    Prvi commit, rjesen jump left right, platforme i jump platforma

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+    private bool _isFacingRight;
+    private CharacterController2D _controller;
+    private float _normalizedHorizontalSpeed;
+
+    public float MaxSpeed = 8;
+    public float SpeedAccelerationOnGround = 10f;
+    public float SpeedAccelerationInAir = 5f;
+
+    public void Start()
+    {
+        _controller = GetComponent<CharacterController2D>();
+        _isFacingRight = transform.localScale.x > 0;
+    }
+    public void Update()
+    {
+        HandleInput();
+
+        var movementFactor = _controller.State.IsGrounded ? SpeedAccelerationOnGround : SpeedAccelerationInAir;
+        _controller.SetHorizontalForce(Mathf.Lerp(_controller.Velocity.x, _normalizedHorizontalSpeed * MaxSpeed, Time.deltaTime * movementFactor));
+
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey("right"))
+        { _normalizedHorizontalSpeed = 1;
+        if (!_isFacingRight)
+            Flip();
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey("left"))
+        {
+            _normalizedHorizontalSpeed = -1;
+            if (_isFacingRight)
+                Flip();
+        }
+        else
+        {
+            _normalizedHorizontalSpeed = 0;
+        }
+
+        if (_controller.CanJump && Input.GetKeyDown(KeyCode.Space) || _controller.CanJump && Input.GetKeyDown("up"))
+        {
+            _controller.Jump();
+        }
+    }
+    
+    private void Flip()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        _isFacingRight = transform.localScale.x > 0;
+    }
+
+}
