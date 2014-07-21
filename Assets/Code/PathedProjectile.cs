18c7fcb6c62a80f3f1172aaecf3dfa64ace7b397
dpoljak <poljak5@gmail.com>
2014-07-22 00:06:06 +0200

    Tut 25# finaliziran

---
+++ Assets/Code/PathedProjectile.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class PathedProjectile : MonoBehaviour
+{
+    private Transform _destination;
+    private float _speed;
+
+    public GameObject DestroyEffect;
+
+    public void Initialize(Transform destination, float speed)
+    {
+        _destination = destination;
+        _speed = speed;
+    }
+
+    public void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _destination.position, Time.deltaTime*_speed);
+        var distanceSquared = (_destination.transform.position - transform.position).sqrMagnitude;
+        if (distanceSquared > 0.1f*0.1f)
+            return;
+
+        if (DestroyEffect != null)
+            Instantiate(DestroyEffect, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Code/PathedProjectileSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.Remoting.Messaging;
+using UnityEngine;
+
+public class PathedProjectileSpawner : MonoBehaviour
+{
+    public Transform Destination;
+    public PathedProjectile Projectile;
+
+    public float Speed;
+    public float FireRate;
+    public GameObject SpawnEffect;
+
+    private float _nextShotInSeconds;
+
+    public void Start()
+    {
+        _nextShotInSeconds = FireRate;
+    }
+
+    public void Update()
+    {
+        if ((_nextShotInSeconds -= Time.deltaTime) > 0)
+            return;
+
+        _nextShotInSeconds = FireRate;
+        var projectile = (PathedProjectile) Instantiate(Projectile, transform.position, transform.rotation);
+        projectile.Initialize(Destination, Speed);
+
+        if (SpawnEffect != null)
+            Instantiate(SpawnEffect, transform.position, transform.rotation);
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (Destination == null)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, Destination.position);
+    }
+}
